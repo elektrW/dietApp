31107acafb1788a731b31440f7414f6c041d599d
elektrW <wojdom1@wp.pl>
2021-08-04 22:18:57 +0200

    Added option 'update' for product

--- src/main/java/pl/wojciechdomagala/dietapp/product/controll/ProductControl.java
+++ src/main/java/pl/wojciechdomagala/dietapp/product/controll/ProductControl.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import pl.wojciechdomagala.dietapp.person.model.PersonData;
 import pl.wojciechdomagala.dietapp.product.model.Product;
 import pl.wojciechdomagala.dietapp.product.service.ProductService;
 
@@ -35,6 +37,15 @@ public class ProductControl {
     public String saveProductInfo(@ModelAttribute Product product) {
         productService.saveProductInfo(product);
         return "redirect:/product";
+    }
+
+    @GetMapping("/showFormForUpdateProduct/{id}")
+    public String showFormForUpdateProduct(@PathVariable long id, Model model) {
+        //get
+        Product product = productService.getProductById(id);
+        //update
+        model.addAttribute("personData", product);
+        return "update_product";
     }
 
 }
