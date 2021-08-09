1d0016bc54cbfcd5f8cfd6295e0c96c3f9467509
elektrW <wojdom1@wp.pl>
2021-08-09 19:18:46 +0200

    Added validation support in saveProductInfo method

--- src/main/java/pl/wojciechdomagala/dietapp/product/controll/ProductControl.java
+++ src/main/java/pl/wojciechdomagala/dietapp/product/controll/ProductControl.java
@@ -3,13 +3,18 @@ package pl.wojciechdomagala.dietapp.product.controll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.wojciechdomagala.dietapp.product.model.Product;
 import pl.wojciechdomagala.dietapp.product.service.ProductService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
@@ -35,9 +40,15 @@ public class ProductControl {
     }
 
     @PostMapping("/saveProductInfo")
-    public String saveProductInfo(@ModelAttribute Product product, Errors errors) {
-        productService.saveProductInfo(product);
-        return "redirect:/product";
+    public String saveProductInfo(@Valid @ModelAttribute Product product, BindingResult result) {
+        if (result.hasErrors()) {
+            List<ObjectError> errors = result.getAllErrors();
+            errors.forEach(err -> System.out.println(err.getDefaultMessage()));
+            return "new_product";
+        } else {
+            productService.saveProductInfo(product);
+            return "redirect:/product";
+        }
     }
 
     @GetMapping("/showFormForUpdateProduct/{id}")
