6a6ed4b9d5fe7ad0e549609e950b4efa543c97e6
elektrW <wojdom1@wp.pl>
2021-08-17 22:36:43 +0200

    Created class with fields needed for his register

--- src/main/java/pl/wojciechdomagala/dietapp/userregister/model/User.java
+++ src/main/java/pl/wojciechdomagala/dietapp/userregister/model/User.java
@@ -1,2 +1,56 @@
-package pl.wojciechdomagala.dietapp.userregister.model;public class User {
+package pl.wojciechdomagala.dietapp.userregister.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "The field cannot be empty")
+    private String username;
+
+    @NotEmpty(message = "The field cannot be empty")
+    private String password;
+
+    @NotEmpty(message = "The field cannot be empty")
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
