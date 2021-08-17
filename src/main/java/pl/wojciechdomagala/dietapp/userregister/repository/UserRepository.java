373965db48b5156a7873afd5bc398ca0c6930a9e
elektrW <wojdom1@wp.pl>
2021-08-17 22:38:02 +0200

    Added repository for register users nad one method to search by using username

--- src/main/java/pl/wojciechdomagala/dietapp/userregister/repository/UserRepository.java
+++ src/main/java/pl/wojciechdomagala/dietapp/userregister/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package pl.wojciechdomagala.dietapp.userregister.repository;public class UserRepository {
+package pl.wojciechdomagala.dietapp.userregister.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.wojciechdomagala.dietapp.userregister.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
 }
