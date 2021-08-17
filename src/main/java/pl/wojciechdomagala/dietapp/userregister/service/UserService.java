5386e3359bd5ba222f5adbeb1e11dc58b20b27d2
elektrW <wojdom1@wp.pl>
2021-08-17 22:41:06 +0200

    Added service

--- src/main/java/pl/wojciechdomagala/dietapp/userregister/service/UserService.java
+++ src/main/java/pl/wojciechdomagala/dietapp/userregister/service/UserService.java
@@ -1,2 +1,23 @@
-package pl.wojciechdomagala.dietapp.userregister.service;public class UserService {
+package pl.wojciechdomagala.dietapp.userregister.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.wojciechdomagala.dietapp.userregister.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
