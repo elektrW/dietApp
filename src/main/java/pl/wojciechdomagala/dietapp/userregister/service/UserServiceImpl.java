48c916ed52d93fc82d089a8202e334c05f6551d4
elektrW <wojdom1@wp.pl>
2021-08-20 21:31:05 +0200

    Implemetation of userservice methods

--- src/main/java/pl/wojciechdomagala/dietapp/userregister/service/UserServiceImpl.java
+++ src/main/java/pl/wojciechdomagala/dietapp/userregister/service/UserServiceImpl.java
@@ -1,2 +1,31 @@
-package pl.wojciechdomagala.dietapp.userregister.service;public class UserServiceImpl {
-}
+//package pl.wojciechdomagala.dietapp.userregister.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import pl.wojciechdomagala.dietapp.userregister.model.User;
+//import pl.wojciechdomagala.dietapp.userregister.repository.UserRepository;
+//
+//@Service
+//public class UserServiceImpl implements UserService {
+//
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    public UserServiceImpl(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public void saveNewUser(User user) {
+//        this.userRepository.save(user);
+//    }
+//
+//    @Override
+//    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+//        this.setPasswordEncoder(passwordEncoder);
+//    }
+//
+//
+//
+//}
