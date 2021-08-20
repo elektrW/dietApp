46a3a62c558684d0a5b6b50d12c8a6bde4a6a015
elektrW <wojdom1@wp.pl>
2021-08-20 21:30:07 +0200

    Created controller to handle user register and log in

--- src/main/java/pl/wojciechdomagala/dietapp/userregister/controll/UserControl.java
+++ src/main/java/pl/wojciechdomagala/dietapp/userregister/controll/UserControl.java
@@ -1,39 +1,46 @@
-package pl.wojciechdomagala.dietapp.userregister.controll;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import pl.wojciechdomagala.dietapp.userregister.model.User;
-import pl.wojciechdomagala.dietapp.userregister.service.UserService;
-
-import javax.validation.Valid;
-
-public class UserController {
-
-    private UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/register")
-    public String register(Model model) {
-        model.addAttribute("user", new User());
-        return "registerForm";
-    }
-
-    @PostMapping("/register")
-    public String addUser(@ModelAttribute @Valid User user,
-                          BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "registerForm";
-        } else {
-            userService.addWithDefaultRole(user);
-        }
-    }
-
-}
+//package pl.wojciechdomagala.dietapp.userregister.controll;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.ObjectError;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import pl.wojciechdomagala.dietapp.userregister.model.User;
+//import pl.wojciechdomagala.dietapp.userregister.service.UserService;
+//
+//import javax.validation.Valid;
+//import java.util.List;
+//
+//@Controller
+//public class UserControl {
+//
+//    private UserService userService;
+//
+//    @Autowired
+//    public UserControl(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/register")
+//    public String register(Model model) {
+//        model.addAttribute("user", new User());
+//        return "registerForm";
+//    }
+//
+//    @PostMapping("/register")
+//    public String addUser(@ModelAttribute @Valid User user,
+//                          BindingResult result) {
+//        if (result.hasErrors()) {
+//            List<ObjectError> errors = result.getAllErrors();
+//            errors.forEach(err -> System.out.println(err.getDefaultMessage()));
+//            return "registerForm";
+//        } else {
+//            userService.saveNewUser(user);
+//            return "registerSuccess";
+//        }
+//    }
+//
+//}
