ab2f3b6fa1b8aa9f67872df8d250c69db7d6a030
elektrW <wojdom1@wp.pl>
2021-08-25 21:07:14 +0200

    Changed method for day and added '+1' to method for month

--- src/main/resources/static/clock.js
+++ src/main/resources/static/clock.js
@@ -1,10 +1,10 @@
 function counter() {
     var today = new Date();
 
-    var day = today.getDay();
+    var day = today.getDate();
     if (day < 10) day = "0" + day;
 
-    var month = today.getMonth();
+    var month = today.getMonth() + 1;
     if (month < 10) month = "0" + month;
 
     var year = today.getFullYear();
